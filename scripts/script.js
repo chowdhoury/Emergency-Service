e3146ab80ad5137d34ad52e79c85dee0ec7d7f92
Raihan <mdr8077@gmail.com>
2025-08-28 03:05:53 +0600

    Copy Functionality Done

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+const hearts = document.getElementsByClassName('heart');
+for (const heart of hearts) {
+    heart.addEventListener('click', function () {
+        const heartCountDisplay = document.getElementById("heartCount");
+        const heartCount = parseInt(heartCountDisplay.innerText);
+        heartCountDisplay.innerText = heartCount + 1;
+    })
+}
+
+const copies = document.getElementsByClassName('copy');
+for (const copy of copies) {
+    copy.addEventListener('click', function (e) {
+        const copyCountDisplay = document.getElementById("copyCount");
+        const copyCount = parseInt(copyCountDisplay.innerText);
+        copyCountDisplay.innerText = copyCount + 1;
+        const name = e.currentTarget.parentNode.parentNode.getElementsByClassName("name")[0].innerText;
+        const number = e.currentTarget.parentNode.parentNode.getElementsByClassName("number")[0].innerText;
+        alert(name + " : " + number + "\n\nHas been copied😊")
+        navigator.clipboard.writeText(number);
+    })
+}
+
+
+const calls = document.getElementsByClassName('call');
+for (const call of calls) {
+    call.addEventListener('click', function () {
+        
+    })
+}
